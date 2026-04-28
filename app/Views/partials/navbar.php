21cbacde323dde8ca55d822823cedf4252644b19
Ali Shaqrany <alishaqrany@gmail.com>
2026-04-28 21:53:19 +0300

    feat(navbar): Add responsive navbar links for improved mobile usability

--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -1,5 +1,13 @@
 <!-- Main Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links (Hamburger menu) -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+    </ul>
+
+    <!-- Right navbar links (Notifications & User) -->
     <ul class="navbar-nav mr-auto">
         <!-- Notifications -->
         <li class="nav-item dropdown">
